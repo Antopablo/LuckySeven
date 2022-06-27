f68b9abb3a367cb1d6cd7a37225bb7f663600e70
= <antony.lefevre@sogeti.com>
2022-06-27 10:58:42 +0200

    architecture with dashboard

---
+++ src/routes/Dashboard.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { currentAccountStore } from "../stores/account";
+</script>
+
+<div class="fx-column">
+  <div>
+    Dashboard - {$currentAccountStore.firstName}
+  </div>
+  <div>
+    Dashboard - {$currentAccountStore.bestScore}
+  </div>
+</div>
